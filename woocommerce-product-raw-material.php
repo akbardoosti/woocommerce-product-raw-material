d5de984d7ab1fb4bdf36edb515c5618eb3740c1b
dousti1371@gmail.com <dousti1371@gmail.com>
2022-05-27 07:38:51 +0430

    Update file names

---
+++ woocommerce-product-raw-material.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 
+ * @package           PDT_Plugin
+ * @author            Akbar Doosti
+ * @copyright         2022 VIP Shop flowers
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Woocommerce product raw material
+ * Plugin URI:        https://wordpress.org/plugins/tags/repository/
+ * Description:       This plugin designed for add raw material to Woocommerce products
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Akbar Doosti
+ * Author URI:        https://wpx93.ir
+ * Text Domain:       pdt-raw-material
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://example.com/my-plugin/
+ * 
+*/
+
